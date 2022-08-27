54b1a029fd15d49b6c0478c8522783d1cb0f6f42
Adejoro Peter <adejoropeter25@gmail.com>
2022-08-27 13:57:28 -0700

    ...

--- src/components/card.component.jsx
+++ src/components/card.component.jsx
@@ -1,20 +1,35 @@
 import React, { useContext, useEffect } from "react";
 import StateManagement from "../contextApi/statemanagement.contextApi";
-import { BsBookmark, BsChevronDoubleLeft, BsFillBookFill, BsFillBookmarkFill } from "react-icons/bs";
+import {
+  BsBookmark,
+  BsChevronDoubleLeft,
+  BsFillBookFill,
+  BsFillBookmarkFill,
+} from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
 const Card = ({ src, dateReleased, title, item }) => {
-  const { setBookMarked, bookmarked, setWatch, toggle, setToggle,items,setItems } =
-    useContext(StateManagement);
-console.log(items.id)
+  const {
+    setBookMarked,
+    bookmarked,
+    setWatch,
+    toggle,
+    setToggle,
+    items,
+    setItems,
+  } = useContext(StateManagement);
+  console.log(bookmarked);
   const addToBookMark = (id) => {
-    items.map(item=>{
-      const ch= item.id === id;
-      // return bookmark.id === id ? {...bookmark,store:!bookmark.store}: bookmark
-      console.log(ch)
-       
-    })
+    const bulk = items.map((item) => {
+      return item.id === id ? { ...item, store: !item.store } : item;
+    });
+    setItems(bulk);
+    if (item.store) {
+      setBookMarked([...bookmarked, item]);
+      console.log(item);
+    } else {
+      console.log(false);
+    }
   };
-
   let navigate = useNavigate();
   const addToWatch = () => {
     setWatch(item);
@@ -31,9 +46,9 @@ console.log(items.id)
         ></video>
         <div
           className="bg-gray-300 rounded-full w-fit h-fit p-3 absolute top-6 right-8 cursor-pointer"
-          onClick={()=>addToBookMark(items.id)}
+          onClick={() => addToBookMark(item.id)}
         >
-          {items.store ? (
+          {!item.store ? (
             <BsFillBookmarkFill size="1rem" />
           ) : (
             <BsBookmark size="1rem" />
